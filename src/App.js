8264e86329023f540addd83e1d2621d858cfe2ca
yosrihmila <yyosri433@gmail.com>
2024-01-07 11:15:01 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import BasicExample from './BasicExample'
+import Heading from './Heading'
+import BodyShorthandExample from './BodyShorthandExample'
+function App() {
+return ( <>
+    <div className='App'>
+    <BasicExample>
+    </BasicExample>
+    <Heading>
+
+    </Heading>
+    <BodyShorthandExample>
+
+    </BodyShorthandExample>
+    <BodyShorthandExample>
+
+</BodyShorthandExample>
+<BodyShorthandExample>
+
+</BodyShorthandExample>
+    </div>
+    </>
+)
+}
+
+export default App
---
+++ src/Heading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Heading() {
+  return (
+    <div>
+      <h1>Hello I'm a heading </h1>
+    </div>
+  )
+}
+
+export default Heading
